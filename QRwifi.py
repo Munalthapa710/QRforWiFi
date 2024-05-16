ee8bd018d85f39f2804d5aac6637e272e13f777b
Munal Thapa <153220135+Munalthapa710@users.noreply.github.com>
2024-05-16 21:26:42 +0545

    Update commentQRwifi.py

--- QRwifi.py
+++ QRwifi.py
@@ -8,8 +8,8 @@ def generate_wifi_qrcode(wifi_id, wifi_password):
     wifi_qr.show()
 
 #username and password
-username = "munamunalhome_2"
-wifi_password = "9869269374"
+username = "munamunalhome_2" #YOUR USERNAME OF WiFi
+wifi_password = "9869269374" #YOUR WiFi PASSWORD
 
 wifi_qr_generate = generate_wifi_qrcode(username, wifi_password)
 
