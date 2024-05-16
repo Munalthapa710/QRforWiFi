a20a670c019147120120a9fe110ce7253616fe68
Munal Thapa <153220135+Munalthapa710@users.noreply.github.com>
2024-05-16 21:23:45 +0545

    Add files via upload

---
+++ QRwifi.py
@@ -0,0 +1,15 @@
+import qrcode
+
+def generate_wifi_qrcode(wifi_id, wifi_password):
+    wifi_url = f"WIFI:T:WPA;S:{wifi_id};P:{wifi_password};"
+
+    # Generate QR code
+    wifi_qr = qrcode.make(wifi_url) 
+    wifi_qr.show()
+
+#username and password
+username = "munamunalhome_2"
+wifi_password = "9869269374"
+
+wifi_qr_generate = generate_wifi_qrcode(username, wifi_password)
+
